3effa5081a1af8ab3399bd95fca4b987824098b5
Prabhashearee <prabhashwaree57@gmail.com>
2022-10-05 21:40:25 +0530

    Create model

---
+++ model/vehecle.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const  VehicleSchema = new mongoose.Schema({
+    Reg_Number :{
+        type:String,
+        required: true,
+        unique:true
+    },
+    Brand :{
+        type:String,
+        required: true,
+        unique:true
+    },
+    Price :{
+        type:String,
+        required: true,
+        unique:true
+    },
+    Fuel_Type:{
+        type:String,
+        required: true,
+    },
+    Date :{
+        type:String,
+        required: true,
+        unique:true
+    },
+    Location :{
+        type:String,
+        required: true,
+        unique:true
+    },
+   
+})
+
+module.exports = mongoose.model('Vehicle',VehicleSchema)
